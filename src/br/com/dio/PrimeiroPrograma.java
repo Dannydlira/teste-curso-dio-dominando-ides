34fe4faf550d417af4b4f7290205cec481436b7d
Dannydlira <dannydlira@gmail.com>
2022-07-08 11:44:20 -0300

    limpando o codigo

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,15 +7,10 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 
 		Gato gato = new Gato();
-		Livros livros = new Livros(); /* essa classe não precisa importar, está dentro do mesmo arquivo */
+		Livros livros = new Livros(); 
 
 		System.out.println(gato);
 		System.out.println(livros);
-
-		/*
-		 * int a = 2; int b = 3; System.out.println("Hello world! " + (a+b));
-		 */
-
 	}
 
 }
